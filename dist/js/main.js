cc88fce0e04b022bb1c4316a5581e3c09aaf9a9e
404kacper <kacper.sionkowski@gmail.com>
2021-09-04 21:29:01 +0200

    Finished basic structure for amenities & added some animations

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,18 @@
+// Elements to grab
 const navbar = document.querySelector('header');
+const galleryContainer = document.querySelector('.gallery-container');
 // Declaration like this otherwise gives error
 var nav_links = [];
 nav_links = document.querySelector('.nav-links').querySelectorAll('li');
 const scrollingElement = document.querySelector(
   '.parallax-perspective-container'
 );
+var whyUsText = document.querySelector('#whyUsText');
 var scrollpos = scrollingElement.scrollTop;
+
+// Animation related variables
+// number = time in ms
+var iconRevealDelay = 350;
 
 // Wait for the page to load
 document.body.classList.add('js-loading');
@@ -17,6 +24,7 @@ function showPage() {
   console.log('Page loaded');
 }
 
+// Navbar animation
 function addTransparentBg() {
   navbar.classList.add('navbar-transform-bg');
   nav_links.forEach((link) => {
@@ -31,7 +39,46 @@ function removeTransparentBg() {
   });
 }
 
+function animateIcons(delay) {
+  var icons = galleryContainer.querySelectorAll('.icon');
+  var textContainers = galleryContainer.querySelectorAll('.text-container');
+  var i = 0;
+
+  function delayedLoop() {
+    setTimeout(function () {
+      icons[i].classList.add('icon-animate');
+      textContainers[i].classList.add('text-container-animate');
+      i++;
+      // Icons length will always be equal the textContainers length
+      if (i < icons.length) {
+        delayedLoop();
+      }
+    }, delay);
+  }
+
+  delayedLoop();
+}
+
+function animateWhyUs() {
+  console.log(whyUsText);
+  whyUsText.classList.add('slide-in-blurred-left')
+}
+
+var iconsFired = false;
+var whyusFired = false;
+// Scrollbar listener
 scrollingElement.addEventListener('scroll', () => {
   scrollpos = scrollingElement.scrollTop;
+  // console.log(scrollpos);
+  // Navbar
   scrollpos >= 100 ? addTransparentBg() : removeTransparentBg();
+  // Icons in info section
+  if (scrollpos >= 200 && iconsFired === false) {
+    animateIcons(iconRevealDelay);
+    iconsFired = true;
+  }
+  if (scrollpos >= 1090 && whyusFired === false) {
+    animateWhyUs();
+    whyusFired = true;
+  }
 });
